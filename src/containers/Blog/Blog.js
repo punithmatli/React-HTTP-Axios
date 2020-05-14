8d57414550b01358dfbbd4ea9011bcc9a19021d8
unknown <punithmatli@gmail.com>
2020-05-14 15:34:44 +0530

    Created Dynamic routes, Replaced Link with NavLink component to be able to add styles to links. Used Switch component to load only one route at a time from the given routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts'
-import { Route,Link } from 'react-router-dom'
+import { Route,NavLink,Switch } from 'react-router-dom'
 import NewPost from "./NewPost/NewPost";
+import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
    
@@ -13,20 +14,29 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname:'/new-post',
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="active"
+                                activeStyle={{
+                                    color:'#fa923f',
+                                    textDecoration:'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash:'#submit',
                                 search:'?quick-submit=true'
-                            }}>NewPost</Link></li>
+                            }}>NewPost</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} />*/}
-                <Route path="/" exact component={Posts} />
+               <Route path="/" exact component={Posts} /> 
+               <Switch>
             <Route path="/new-post" exact component={NewPost} />
-                
+            <Route path="/:id" exact component={FullPost} />    
+            </Switch>    
             </div>
         );
     }
