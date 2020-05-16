fc39fa299a198d178487736a8703aea1f3b68d11
unknown <punithmatli@gmail.com>
2020-05-16 14:58:16 +0530

    Implemented lazy loading and added guard to protect against 404 error which occurs when the route is not found

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts'
+import asyncComponent from '../../hoc/asyncComponent'
 import { Route,NavLink,Switch,Redirect } from 'react-router-dom'
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost') 
+}) ;
 class Blog extends Component {
-   
+    state = {
+        auth:true
+    }
+
     render () {
      
         return (
@@ -33,9 +40,12 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} />*/}
                
                <Switch>
-               <Route path="/new-post" exact component={NewPost} />
+               {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost} /> : null }
                <Route path="/posts" component={Posts} /> 
-               <Redirect from='/' to='/posts' />
+               <Route render = {() => <h1>Not Found!</h1>} />
+               {//<Redirect from='/' to='/posts' />
+               }
+               
             </Switch>    
             </div>
         );
